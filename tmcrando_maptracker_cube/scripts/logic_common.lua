b03c105529be16212591656be7718862b5b15ca3
cubesolver111 <cubesolver111@gmail.com>
2019-09-28 01:40:48 -0500

    add functions for dhc options

--- tmcrando_maptracker_cube/scripts/logic_common.lua
+++ tmcrando_maptracker_cube/scripts/logic_common.lua
@@ -17,3 +17,31 @@ function hasNoGust()
     return 1
   end
 end
+
+function TwoElements()
+  if Tracker:ProviderCountForCode("earth") > 0 and Tracker:ProviderCountForCode("fire") > 0
+  or Tracker:ProviderCountForCode("earth") > 0 and Tracker:ProviderCountForCode("water") > 0
+  or Tracker:ProviderCountForCode("earth") > 0 and Tracker:ProviderCountForCode("wind") > 0
+  or Tracker:ProviderCountForCode("fire") > 0 and Tracker:ProviderCountForCode("water") > 0
+  or Tracker:ProviderCountForCode("fire") > 0 and Tracker:ProviderCountForCode("wind") > 0
+  or Tracker:ProviderCountForCode("water") > 0 and Tracker:ProviderCountForCode("wind") > 0
+  then
+    return 1
+  end
+end
+
+function ThreeElements()
+  if Tracker:ProviderCountForCode("earth") > 0 and Tracker:ProviderCountForCode("fire") > 0 and Tracker:ProviderCountForCode("water") > 0
+  or Tracker:ProviderCountForCode("earth") > 0 and Tracker:ProviderCountForCode("fire") > 0 and Tracker:ProviderCountForCode("wind") > 0
+  or Tracker:ProviderCountForCode("fire") > 0 and Tracker:ProviderCountForCode("water") > 0 and Tracker:ProviderCountForCode("wind") > 0
+  then
+    return 1
+  end
+end
+
+function FourElements()
+  if Tracker:ProviderCountForCode("earth") > 0 and Tracker:ProviderCountForCode("fire") > 0 and Tracker:ProviderCountForCode("water") > 0 and Tracker:ProviderCountForCode("wind") > 0
+  then
+    return 1
+  end
+end
